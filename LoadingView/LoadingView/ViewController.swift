0a196d71c43cbeba62e0b9bcbbc511642b6f4fe9
Younger <mac@MacdeMacBook-Air.local>
2016-10-28 20:11:53 +0800

    init

---
+++ LoadingView/LoadingView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LoadingView
+//
+//  Created by Mac on 2016/10/28.
+//  Copyright © 2016年 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
